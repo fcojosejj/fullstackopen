2ba74fa47b2b8b883fda3c7799b17f0425ff68c8
Francisco José Jordán Jiménez <fcojosejordanjimenez@gmail.com>
2025-02-18 11:37:32 +0100

    part4 completed

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,8 +1,19 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
+
+/* const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+} */
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -11,39 +22,55 @@ blogsRouter.get('/:id', async (request, response) => {
   response.json(blog)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
   const body = request.body
+  const user = request.user
 
   const newBlog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ? body.likes : 0,
+    user: user.id
   })
 
   const blog = await newBlog.save()
+  user.blogs = user.blogs.concat(blog._id)
+  await user.save()
   response.status(201).json(blog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
-  if (!deletedBlog) response.status(404).end()
-  else response.status(204).end()
+blogsRouter.delete('/:id', middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (user.id.toString() !== blog.user.toString()) response.status(401).json({ error: 'unauthorized' })
+  else if (!blog) response.status(404).end()
+  else {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  }
 })
 
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
   const body = request.body
 
-  const blog = {
+  const newblog = {
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   }
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  if (!updatedBlog) response.status(404).end()
-  else response.json(updatedBlog)
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (user.id.toString() !== blog.user.toString()) response.status(401).json({ error: 'unauthorized' })
+  else if (!blog) response.status(404).end()
+  else {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newblog, { new: true })
+    response.status(200).json(updatedBlog)
+  }
 })
 
 module.exports = blogsRouter
--- part4/blogs/tests/blogs_api.test.js
+++ part4/blogs/tests/blogs_api.test.js
@@ -7,9 +7,10 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blogs')
-const blogs = require('../models/blogs')
+const User = require('../models/user')
 
 beforeEach(async () => {
+  await User.deleteMany({})
   await Blog.deleteMany({})
 
   const blogs = helper.initialBlogs
@@ -24,16 +25,16 @@ describe('obtain blogs', () => {
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  
+
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
-  
+
   test('blogs have a property called id', async () => {
     const response = await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  
+
     response.body.forEach(blog => {
       assert.ok(Object.hasOwn(blog, 'id'))
     })
@@ -48,106 +49,208 @@ describe('create a blog', () => {
       url: 'https://www.new.com',
       likes: 10
     }
-  
+
+    const newUser = {
+      username: 'root',
+      name: 'Francisco José Jordán Jiménez again',
+      password: 'solana1234',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'solana1234' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-  
-      const response = await helper.blogsInDb()
-      assert.strictEqual(response.length, helper.initialBlogs.length + 1)
+
+    const response = await helper.blogsInDb()
+    assert.strictEqual(response.length, helper.initialBlogs.length + 1)
   })
-  
+
   test('if a blog does not have like property, it is set to 0', async () => {
     const newBlog = {
       title: 'New blog',
       author: 'New author',
       url: 'https://www.new.com',
     }
-  
+
+    const newUser = {
+      username: 'root',
+      name: 'Francisco José Jordán Jiménez again',
+      password: 'solana1234',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'solana1234' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-  
+
     const response = await helper.blogsInDb()
     assert.strictEqual(response[response.length - 1].likes, 0)
   })
-  
+
   test('if a blog does not have title or url property, it is not created', async () => {
     const newBlog = {
       author: 'New author',
       likes: 10
     }
-  
+
+    const newUser = {
+      username: 'root',
+      name: 'Francisco José Jordán Jiménez again',
+      password: 'solana1234',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'solana1234' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
       .send(newBlog)
       .expect(400)
       .expect('Content-Type', /application\/json/)
-  
+
     const response = await helper.blogsInDb()
     assert.strictEqual(response.length, helper.initialBlogs.length)
   })
 })
 
 describe('delete a blog', () => {
-  test('a non existing blog cannot be deleted', async () => {
-    const nonExistingId = await helper.nonExistingId()
-    const response = await api
-      .delete(`/api/blogs/${nonExistingId}`)
-      .expect(404)
-  })
-
   test('a blog can be deleted', async () => {
+    const newBlog = {
+      title: "test",
+      author: "Francisco José Jordán Jiménez",
+      url: "https://www.test.es/es/",
+      likes: 6
+    }
+
+    const newUser = {
+      username: 'root',
+      name: 'Francisco José Jordán Jiménez again',
+      password: 'solana1234',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'solana1234' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+
     const blogsBeforeDelete = await helper.blogsInDb()
-  
+
     const response = await api
       .delete(`/api/blogs/${blogsBeforeDelete[blogsBeforeDelete.length - 1].id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
       .expect(204)
-  
+
     const response2 = await helper.blogsInDb()
     assert.strictEqual(response2.length, blogsBeforeDelete.length - 1)
   })
 })
 
 describe('update a blog', () => {
-  test('a non existing blog cannot be updated', async () => {
-    const newBlog = {
-      title: "Més que un club",
-      author: "Francisco José Jordán Jiménez",
-      url: "https://www.fcbarcelona.es/es/",
-      likes: 6
-    }
-  
-    const nonExistingId = await helper.nonExistingId()
-    await api
-      .put(`/api/blogs/${nonExistingId}`)
-      .send(newBlog)
-      .expect(404)
-  })
-
   test('a blog can be updated', async () => {
     const newBlog = {
-      title: "Més que un club",
+      title: "test",
       author: "Francisco José Jordán Jiménez",
-      url: "https://www.fcbarcelona.es/es/",
+      url: "https://www.test.es/es/",
       likes: 6
     }
-  
-    const blogs = await helper.blogsInDb()
-  
+
+    const newUser = {
+      username: 'root',
+      name: 'Francisco José Jordán Jiménez again',
+      password: 'solana1234',
+    }
+
     await api
-      .put(`/api/blogs/${blogs[0].id}`)
-      .send(newBlog)
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'solana1234' })
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogs = await helper.blogsInDb()
+
+    const updatedBlog = {
+      title: "test updated",
+      author: "Francisco José Jordán Jiménez",
+      url: "https://www.test.es/es/",
+      likes: 60
+    }
+
+    await api
+      .put(`/api/blogs/${blogs[blogs.length - 1].id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(updatedBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
     const response = await helper.blogsInDb()
-    assert.strictEqual(response[0].title, 'Més que un club')
-    assert.strictEqual(response[0].likes, 6)
+    assert.strictEqual(response[response.length - 1].title, 'test updated')
+    assert.strictEqual(response[response.length - 1].likes, 60)
   })
 })
 
